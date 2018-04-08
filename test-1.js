a01c4fe25ab08419d458277fdaf02ca5168af417
DanhVT <danhvt@blockchainlabs.asia>
2018-04-08 19:50:35 +0100

    fix javascript

---
+++ test-1.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var listSection = document.getElementsByClassName("section");
+    console.log(listSection);
+    for (var i = 0; i < listSection.length; i++) {
+        (function (_section) {
+            
+            var _p = _section.getElementsByTagName('section')[0];
+            var _button = _section.getElementsByTagName('a')[0];
+            _button.addEventListener('click', function (e) {
+                document.querySelectorAll('.section').forEach(function (a) {
+                    a.getElementsByTagName('section')[0].classList.remove('open');
+                    a.getElementsByTagName('a')[0].innerHTML = "Show Less";
+                });
+
+                var target = e.target || e.srcElement;
+                _p.classList.toggle('open');
+                _button.innerHTML = "Show More";
+            });
+        }(listSection[i]))        
+    }    
+});
