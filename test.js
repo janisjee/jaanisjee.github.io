612ca1e20c57cf09d3b8fc0c808b51d59ebfbfe9
janisjee <89.jekabsons@gmail.com>
2018-04-09 14:51:34 +0100

    fixed html and css

--- test.js
+++ test.js
@@ -1,70 +1,21 @@
-var content = document.getElementById("xp");
-var button = document.getElementById("show-more");
-var content1 = document.getElementById("xp1");
-var button1 = document.getElementById("show-more1");
-var content2 = document.getElementById("xp2");
-var button2 = document.getElementById("show-more2");
-var content2 = document.getElementById("xp3");
-var button2 = document.getElementById("show-more3");
+document.addEventListener("DOMContentLoaded", function (event) {
+    var listSection = document.getElementsByClassName("section");
+    console.log(listSection);
+    for (var i = 0; i < listSection.length; i++) {
+        (function (_section) {
+            
+            var _p = _section.getElementsByTagName('section')[0];
+            var _button = _section.getElementsByTagName('a')[0];
+            _button.addEventListener('click', function (e) {
+                document.querySelectorAll('.section').forEach(function (a) {
+                    a.getElementsByTagName('section')[0].classList.remove('open');
+                    a.getElementsByTagName('a')[0].innerHTML = "Show More";
+                });
 
-button.onclick = function(){
-
-    if(content.className == "open"){
-        //shrink the box
-        content.className = "";
-        button.innerHTML = "Show More";
-        content1.className = "";
-        button1.innerHTML = "Show More";
-        content2.className = "";
-        button2.innerHTML = "Show More";
-    } else {
-        //expand the box
-        content.className = "open";
-        button.innerHTML = "Show Less";
-        content1.className = "";
-        button1.innerHTML = "Show More";
-        content2.className = "";
-        button2.innerHTML = "Show More";
-    }
-};
-button1.onclick = function(){
-
-    if(content1.className == "open"){
-        //shrink the box
-        content.className = "";
-        button.innerHTML = "Show More";
-        content1.className = "";
-        button1.innerHTML = "Show More";
-        content2.className = "";
-        button2.innerHTML = "Show More";
-    } else {
-        //expand the box
-        content.className = "";
-        button.innerHTML = "Show More";
-        content1.className = "open";
-        button1.innerHTML = "Show Less";
-        content2.className = "";
-        button2.innerHTML = "Show More";
-    }
-};
-button2.onclick = function(){
-
-    if(content2.className == "open"){
-        //shrink the box
-        content.className = "";
-        button.innerHTML = "Show More";
-        content1.className = "";
-        button1.innerHTML = "Show More";
-        content2.className = "";
-        button2.innerHTML = "Show More";
-    } else {
-        //expand the box
-        content.className = "";
-        button.innerHTML = "Show More";
-        content1.className = "";
-        button1.innerHTML = "Show More";
-        content2.className = "open";
-        button2.innerHTML = "Show Less";
-    }
-
-};
+                var target = e.target || e.srcElement;
+                _p.classList.toggle('open');
+                _button.innerHTML = "";
+            });
+        }(listSection[i]))        
+    }    
+});
